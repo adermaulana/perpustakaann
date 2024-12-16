9b7f85cd4b367e6fd787206d04318bcb930a2701
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-12-16 20:40:10 +0800

    library

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,70 +1,64 @@
-  <footer id="footer" class="footer dark-background">
-
+<footer id="footer" class="footer dark-background">
     <div class="container footer-top">
-      <div class="row gy-4">
-        <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Bootslander</span>
-          </a>
-          <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
-          </div>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
-          </div>
-        </div>
+        <div class="row gy-4">
+            <div class="col-lg-4 col-md-6 footer-about">
+                <a href="/" class="logo d-flex align-items-center">
+                    <span class="sitename">Perpustakaan</span>
+                </a>
+                <div class="footer-contact pt-3">
+                    <p>Jl Perintis Kemerdekaan</p>
+                    <p>Makassar, Indonesia</p>
+                    <p class="mt-3"><strong>Phone:</strong> <span>+62 812-4072-4040</span></p>
+                    <p><strong>Email:</strong> <span>perpustakaan@perpustakaan.org</span></p>
+                </div>
+                <div class="social-links d-flex mt-4">
+                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 footer-links">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Collections</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Digital Resources</a></li>
+                </ul>
+            </div>
 
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
-        </div>
+            <div class="col-lg-2 col-md-3 footer-links">
+                <h4>Library Services</h4>
+                <ul>
+                    <li><a href="#">Book Borrowing</a></li>
+                    <li><a href="#">Research Assistance</a></li>
+                    <li><a href="#">Community Programs</a></li>
+                    <li><a href="#">Children's Library</a></li>
+                    <li><a href="#">Online Catalog</a></li>
+                </ul>
+            </div>
 
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
+            <div class="col-lg-4 col-md-12 footer-newsletter">
+                <h4>Library Newsletter</h4>
+                <p>Subscribe to receive updates about new arrivals, upcoming events, and library resources!</p>
+                <form action="forms/newsletter.php" method="post" class="php-email-form">
+                    <div class="newsletter-form"><input type="email" name="email"><input type="submit"
+                            value="Subscribe"></div>
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your subscription request has been processed. Thank you!</div>
+                </form>
+            </div>
         </div>
-
-        <div class="col-lg-4 col-md-12 footer-newsletter">
-          <h4>Our Newsletter</h4>
-          <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-          <form action="forms/newsletter.php" method="post" class="php-email-form">
-            <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your subscription request has been sent. Thank you!</div>
-          </form>
-        </div>
-
-      </div>
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Bootslander</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">City Central Library</strong> <span>All Rights
+                Reserved</span></p>
+        <div class="credits">
+            Website Design by City Central Library Digital Services Team
+        </div>
     </div>
-
-  </footer>
+</footer>
